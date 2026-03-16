b9f00bfada8b8e2c2ccd9b366a108049d5d0d8b7
Courage Dei <couragedei555@gmail.com>
2026-03-16 10:17:21 +0000

    feat: Run the application using Socket.IO.

--- run.py
+++ run.py
@@ -4,9 +4,9 @@ import os
 # Add src to python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
-from app import app, db
+from app import app, db, socketio
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    socketio.run(app, debug=True)
